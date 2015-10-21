9913df398771c5ea54588af2237670ec4229bc4c
Yuliya <yuliya_tukaila@epam.com>
2015-10-21 12:04:34 +0300

    sec commit

--- .NET/Calculator/Calc.cs
+++ .NET/Calculator/Calc.cs
@@ -5,12 +5,12 @@
     {
         public static int Sum(int a, int b)
         {
-            return a + b;
+            return a - b;
         }
 
         public static int Substract(int a, int b)
         {
-            return a - b;
+            return a + b;
         }
         
         public static int Multiply(int a, int b)
